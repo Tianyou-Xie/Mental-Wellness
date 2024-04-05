bf6bef70d254800f2d064c606e0e2d96666d7039
Tianyou Xie <tianyouxie001@gmail.com>
2024-04-04 18:37:02 -0700

    Modified record output

--- scripts/recordlist.js
+++ scripts/recordlist.js
@@ -21,100 +21,6 @@ function isYesterday(timestamp) {
     return timestamp.toDateString() === yesterday.toDateString();
 }
 
-// Version 01
-// Function to retrieve emotion records from Firebase Firestore
-// function retrieveEmotionRecords() {
-//     const recordsContainer = document.getElementById('emotionRecords');
-
-//     // Clear previous data
-//     recordsContainer.innerHTML = '';
-
-//     firebase.auth().onAuthStateChanged(user => {
-//         if (user) {
-//             console.log(user.uid); //print the uid in the browser console
-//             // Fetch emotion records from Firestore
-//             db.collection("users").doc(user.uid).collection("emotion")
-//                 .orderBy("timestamp", "desc")
-//                 .get()
-//                 .then((querySnapshot) => {
-//                     let currentDay = null;
-
-//                     querySnapshot.forEach((doc) => {
-//                         const data = doc.data();
-//                         const timestamp = data.timestamp.toDate();
-//                         const emotionValue = data.value;
-
-//                         // Format timestamp
-//                         const formattedTimestamp = formatTimestamp(timestamp);
-
-//                         // Determine day label
-//                         let dayLabel = '';
-//                         if (isToday(timestamp)) {
-//                             dayLabel = 'Today';
-//                         } else if (isYesterday(timestamp)) {
-//                             dayLabel = 'Yesterday';
-//                         } else {
-//                             dayLabel = formatDate(timestamp);
-//                         }
-
-//                         // Create HTML elements for record
-//                         if (dayLabel !== currentDay) {
-//                             currentDay = dayLabel;
-//                             const dayHeader = document.createElement('h2');
-//                             dayHeader.textContent = dayLabel;
-//                             recordsContainer.appendChild(dayHeader);
-//                         }
-
-//                         const recordItem = document.createElement('div');
-//                         recordItem.innerHTML = `${formattedTimestamp}\t${getEmotionText(emotionValue)}`;
-//                         recordsContainer.appendChild(recordItem);
-//                     });
-//                 })
-//                 .catch((error) => {
-//                     console.error("Error retrieving emotion records: ", error);
-//                 });
-//         } else {
-//             // No user is signed in.
-//             console.log("No user is signed in");
-//             window.location.href = "login.html";
-//         }
-//     });
-// }
-
-// // Function to get the corresponding emotion text with icon
-// function getEmotionText(value) {
-//     let emotionText = '';
-//     let iconClass = '';
-//     switch (value) {
-//         case 1:
-//             emotionText = 'Very Sad';
-//             iconClass = 'fa-face-sad-cry';
-//             break;
-//         case 2:
-//             emotionText = 'Sad';
-//             iconClass = 'fa-face-sad-tear';
-//             break;
-//         case 3:
-//             emotionText = 'Normal';
-//             iconClass = 'fa-face-meh';
-//             break;
-//         case 4:
-//             emotionText = 'Happy';
-//             iconClass = 'fa-face-smile';
-//             break;
-//         case 5:
-//             emotionText = 'Very Happy';
-//             iconClass = 'fa-face-laugh-beam';
-//             break;
-//         default:
-//             emotionText = '';
-//             iconClass = '';
-//     }
-//     return `&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span>${emotionText}</span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<i class="fa-regular ${iconClass} fa-3x"></i><br><br>`;
-// }
-
-// // Call function to retrieve and display emotion records
-// retrieveEmotionRecords();
 
 // Function to retrieve emotion records from Firebase Firestore
 function retrieveEmotionRecords() {
@@ -191,19 +97,19 @@ function getEmotionText(value) {
     let emotionText = '';
     switch (value) {
         case 1:
-            emotionText = 'Very Sad';
+            emotionText = 'Awful';
             break;
         case 2:
             emotionText = 'Sad';
             break;
         case 3:
-            emotionText = 'Normal';
+            emotionText = 'Neutral';
             break;
         case 4:
             emotionText = 'Happy';
             break;
         case 5:
-            emotionText = 'Very Happy';
+            emotionText = 'Great';
             break;
         default:
             emotionText = '';
@@ -216,19 +122,19 @@ function getEmotionIcon(value) {
     let iconClass = '';
     switch (value) {
         case 1:
-            iconClass = 'fa-face-sad-cry';
+            iconClass = 'fa-face-sad-cry red';
             break;
         case 2:
-            iconClass = 'fa-face-sad-tear';
+            iconClass = 'fa-face-sad-tear orange';
             break;
         case 3:
-            iconClass = 'fa-face-meh';
+            iconClass = 'fa-face-meh yellow';
             break;
         case 4:
-            iconClass = 'fa-face-smile';
+            iconClass = 'fa-face-smile light-green';
             break;
         case 5:
-            iconClass = 'fa-face-laugh-beam';
+            iconClass = 'fa-face-laugh-beam green';
             break;
         default:
             iconClass = '';
